149f6e8c2e8d5c18997d75bd6834b1c3079c0f54
James Brown <jamesbrown173@gmail.com>
2024-11-14 13:12:54 +0700

    f

--- blue-white-moon-landing-page/app/layout.js
+++ blue-white-moon-landing-page/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
 import SocialsFooter from "@/components/SocialsFooter";
 
 const geistSans = localFont({
--- blue-white-moon-landing-page/app/page.js
+++ blue-white-moon-landing-page/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import BackgroundSemiCircleWithAnimations from "@/components/BackgroundSemiCircleWithAnimations";
 import DarkButton from "@/components/DarkButton";
